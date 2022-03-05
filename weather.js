fc609e7df3d69ee5143c7cb2f0f0250b6acdb9bf
Meekwest <meekwest@mail.ru>
2022-03-05 22:46:34 +0300

    error handling added

--- weather.js
+++ weather.js
@@ -8,29 +8,43 @@ const saveToken = async (token) => {
 	if (!token.length) {
 		printError('Не передан токен');
 		return;
-	}
+	};
 	try {
 		await saveKeyValue(TOKEN_DICTIONARY.token, token);
 		printSuccess('Токен сохранен');
 	} catch (e) {
 		printError(e.message);
-	}
-}
+	};
+};
+
+const getForecast = async () => {
+	try {
+		const weather = await getWeather(process.env.CITY);
+		console.log(weather);
+	} catch (e) {
+		if (e?.response?.status == 404) {
+			printError('Неверно указан город');
+		} else if (e?.response?.status == 401) {
+			printError('Неверно указан токен');
+		} else {
+			printError(e.message);
+		};
+	};
+};
 
 const initCLI = () => {
-	const args = getArgs(process.argv)
+	const args = getArgs(process.argv);
 
 	if (args.h) {
 		printHelp();
-	}
+	};
 	if (args.s) {
-		//
-	}
+		
+	};
 	if (args.t) {
 		return saveToken(args.t)
-	}
-	getWeather('moscow');
-	// Вывести погоду
+	};
+	getForecast();
 };
 
 initCLI();
