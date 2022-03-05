f3ab7d124f1c0e77d197daf1154f9b37b755dc30
Meekwest <meekwest@mail.ru>
2022-03-05 19:20:09 +0300

    file system operations added

--- weather.js
+++ weather.js
@@ -1,7 +1,16 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js'
-import { printHelp } from './services/log.service.js';
+import { printHelp, printError, printSuccess } from './services/log.service.js';
+import { saveKeyValue } from './services/storage.service.js';
 
+const saveToken = async (token) => {
+	try {
+		await saveKeyValue('token', token);
+		printSuccess('Токен сохранен');
+	} catch (e) {
+		printError(e.message);
+	}
+}
 
 const initCLI = () => {
 	const args = getArgs(process.argv)
@@ -10,10 +19,10 @@ const initCLI = () => {
 		printHelp();
 	}
 	if (args.s) {
-		// Сохранить город
+		//
 	}
 	if (args.t) {
-		// Сохранить токен
+		return saveToken(args.t)
 	}
 	// Вывести погоду
 };
