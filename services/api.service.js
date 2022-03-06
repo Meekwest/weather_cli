4caa2c1a60bf85a2afd2e385e787a13d98b5ede3
Meekwest <meekwest@mail.ru>
2022-03-06 13:19:19 +0300

    end of dev, saveCity and foreCast added

--- services/api.service.js
+++ services/api.service.js
@@ -1,8 +1,28 @@
 import axios from 'axios';
-import https from 'https';
 import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js';
 
-
+const getIcon = (icon) => {
+	switch (icon.slice(0, -1)) {
+		case '01':
+			return '☀️';
+		case '02':
+			return '🌤️';
+		case '03':
+			return '☁️';
+		case '04':
+			return '☁️';
+		case '09':
+			return '🌧️';
+		case '10':
+			return '🌦️';
+		case '11':
+			return '🌩️';
+		case '13':
+			return '❄️';
+		case '50':
+			return '🌫️';
+	}
+};
 const getWeather = async (city) => {
 	const token = process.env.TOKEN ?? await getKeyValue(TOKEN_DICTIONARY.token);
 	if (!token) {
@@ -14,10 +34,10 @@ const getWeather = async (city) => {
 			q: city,
 			appid: token,
 			lang: 'ru',
-			unitrs: 'metric'
+			units: 'metric'
 		}
 	});
 	return data;
 };
 
-export { getWeather };
+export { getWeather, getIcon };
